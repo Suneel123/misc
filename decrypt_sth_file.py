e286a3bc885df10e6af80794b36043b03b61002d
suneel123 <dsuneel1@gmail.com>
2020-10-07 10:19:15 +0530

    Added docstring

--- decrypt_sth_file.py
+++ decrypt_sth_file.py
@@ -1,4 +1,26 @@
-# Decrypt stash (.sth) file
+"""Decrypt stash (.sth) file
+
+IBM HTTP Server (IHS) uses cms keystore.
+When keystore of type cms is created using ikeyman tool, it generates 4 files
+
+1. *.kdb: contains the private keys, certificates and CA's. It is encrypted
+          with a password that can be stashed in the *.sth file.
+2. *.rdb: contains information about outstanding certificate requests. It's
+          critical that you maintain this 1:1 with KDB.
+3. *.crl: contains revocation info. It is generally not interesting, unless it
+          gets corrupted/mismatched in which case it can cause runtime errors.
+4. *.sth: is a way to store obfuscated password to a file. Runtime tools can
+          use this password instead of prompting for one interactively. It
+          obviously has to be protected if you have private keys in the
+          corresponding KDB.
+
+You should treat all these files as a single atomic set of files and never copy
+a subset. You always reference *.kdb from configuration or with the certificate
+management tools.
+
+The set of 4 files all put together is similar to PKCS12 file in other tools.
+"""
+
 from pathlib import Path
 
 
